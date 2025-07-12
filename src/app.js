3ec6a357f77d68a66e7ae2df9c7538ef34c7c53e
sanyals300 <sanyals300@gmail.com>
2025-07-12 13:13:52 +0530

    Learnt about next function

--- src/app.js
+++ src/app.js
@@ -22,9 +22,27 @@ app.patch('/Sabya', (req, res) => {
   res.send('Sabya modified!');
 });
  
-app.use("/Sabya",(req, res) => {
-  res.send('Hello, GTS!');
-});
+app.use("/SabyaEggroll",(req, res, next) => {
+  console.log("How to make a egg roll");
+  next();  
+},
+(req, res, next) => {
+  console.log("Get yourself a overnight stale roti");
+  next();
+},
+(req, res, next) => {
+  console.log("Crack an egg over it and do 7.5 anticlockwise rotations over it while chanting Sabya is the best");
+  next();
+},
+(req, res, next) => {
+  console.log("Sprinkle some pepper,salt and some green chillies and onions over it while chanting Sabya is the best");
+  next();
+},
+(req, res, next) => {
+  console.log("Now squeeze some ketchup and chilli sauce over it, roll it and eat it while chanting Sabya is the best");
+  res.send("Egg roll is ready! Enjoy!");
+},
+);
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
