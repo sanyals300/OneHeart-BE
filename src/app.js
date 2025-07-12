531507381241bedbdea3cd2916b1c55f15a6e611
sanyals300 <sanyals300@gmail.com>
2025-07-12 12:48:55 +0530

    Used HTTP Methods

--- src/app.js
+++ src/app.js
@@ -2,9 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req, res) => {
+app.get('/Sabya', (req, res) => {
+  res.send('Hello, Sabya!');
+});
+
+app.post('/Sabya', (req, res) => {
+  res.send('Sabya submitted!');
+});
+
+app.delete('/Sabya', (req, res) => {
+  res.send('Sabya deleted!');
+});
+
+app.put('/Sabya', (req, res) => {
+  res.send('Sabya updated!');
+});
+
+app.patch('/Sabya', (req, res) => {
+  res.send('Sabya modified!');
+});
+ 
+app.use("/Sabya",(req, res) => {
   res.send('Hello, GTS!');
-}); 
+});
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
